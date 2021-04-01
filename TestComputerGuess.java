9e288ea57a89b0a695d0afd04a13aa6102f284dc
KajalTomar <kajaltomar09@gmail.com>
2021-04-01 11:41:41 -0500

    oops saved the test file in the wrong folder, added it to the correct folder

---
+++ TestComputerGuess.java
@@ -0,0 +1,27 @@
+//--------------------------------------------------------
+// CLASS: TestComputerGuess.java
+//
+// Author: Kajal Tomar, 7793306
+//
+// REMARKS: the purpose of this class is to test the
+// the guesses made by the ComputerPlayer class.
+//
+//--------------------------------------------------------
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import sun.awt.dnd.SunDragSourceContextPeer;
+
+public class TestComputerGuess{
+
+    @Test
+    public void noCards() {
+        // a computer player has no cards, then canAnser should returh null
+        ComputerPlayer bot = new ComputerPlayer();
+        player = new HumanPlayer();
+
+        Guess guess = new Guess(new Person("Bobby"),new Location("Kitchen"),Weapon("Gun"),false);
+
+        assertNull( bot.canAnswer(guess, player));
+    }
+}
