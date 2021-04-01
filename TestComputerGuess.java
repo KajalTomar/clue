dd9f402612cd315b5169bc22d8520911a79f6767
KajalTomar <kajaltomar09@gmail.com>
2021-04-01 12:30:46 -0500

    added the exactly one test

--- TestComputerGuess.java
+++ TestComputerGuess.java
@@ -23,4 +23,34 @@ public class TestComputerGuess{
 
         assertNull(bot.canAnswer(guess, player));
     }
+
+    @Test
+    public void exactlyOneCard() {
+        // If a computer player has exactly one card from a guess, canAnswer should return that card.
+
+        // these are the cards the computer will have
+        Card joe = new Suspect("Joe");
+        Card livingRoom = new Location("Living room");
+        Card knife = new Weapon("Knife");
+
+        ComputerPlayer bot = new ComputerPlayer();
+        HumanPlayer player = new HumanPlayer();
+
+        bot.setCard(joe);
+        bot.setCard(livingRoom);
+        bot.setCard(knife);
+
+        // has only the suspect
+        Guess guess0 = new Guess(joe,new Location("Kitchen"), new Weapon("Gun"),false);
+        assertEquals(joe,bot.canAnswer(guess0,player));
+
+        // has only the location
+        Guess guess1 = new Guess(new Suspect("Bobby"),livingRoom, new Weapon("Gun"),false);
+        assertEquals(livingRoom,bot.canAnswer(guess1,player));
+
+        // has only the weapon
+        Guess guess2 = new Guess(new Suspect("Bobby"),new Location("Kitchen"),knife,false);
+        assertEquals(knife,bot.canAnswer(guess2,player));
+    }
+
 }
